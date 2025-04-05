cba225a46b5dad47462db246f9ab4196559d257f
Peter Atkinson <peterjatkinson@gmail.com>
2025-04-05 16:00:35 +0100

    Added AI timeline app

---
+++ src/app/timeline/page.js
@@ -0,0 +1,9 @@
+import CampaignIdeas from '@/components/AITimeline';
+
+export default function TimelinePage() {
+  return (
+    <div className="container mx-auto py-8">
+      <AITimeline />
+    </div>
+  );
+}
