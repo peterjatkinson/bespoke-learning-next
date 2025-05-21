3e1b8b868e6a10bbad0d2f51bb11a2d031d55624
peterjatkinson <peterjatkinson@gmail.com>
2025-05-21 11:25:23 +0100

    Revert to Insendi wrapper

--- src/components/ResizeWrapper.js
+++ src/components/ResizeWrapper.js
@@ -1,27 +1,12 @@
-// ResizeWrapper.js
 "use client";
 
 import React, { useEffect } from 'react';
-import { initiateLtiAutoResize } from './resizeHelper'; // Ensure this path is correct
+import { initiateAutoResize } from './resizeHelper';
 
 const ResizeWrapper = ({ children }) => {
   useEffect(() => {
-    let cleanupFunction = null;
-
-    if (typeof window !== 'undefined') {
-      if (window.parent && window.parent !== window) {
-        console.log("LTI iFrame Resizer: Initializing for iframe environment.");
-        cleanupFunction = initiateLtiAutoResize();
-      } else {
-        console.log("LTI iFrame Resizer: Not in an iframe or parent is self. Skipping resize logic.");
-      }
-    }
-
-    return () => {
-      if (cleanupFunction) {
-        cleanupFunction();
-      }
-    };
+    // Start observing for resize changes when the component mounts
+    initiateAutoResize();
   }, []);
 
   return <div>{children}</div>;
