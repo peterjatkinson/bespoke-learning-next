e468f844041cea2df734cc69f8f362e472e0634b
GitHub Action <action@github.com>
2025-05-21 16:41:32 +0000

    chore: update site map and landing pages

--- src/app/test-apps/page.js
+++ src/app/test-apps/page.js
@@ -26,6 +26,11 @@ export default function LandingPage() {
             </a>
           </li>
             <li className="bg-white rounded-lg shadow p-4 hover:shadow-lg transition-shadow duration-200">
+            <a href="/test-apps/bad-example" className="text-lg font-medium text-indigo-600 hover:text-indigo-800">
+              Bad Example
+            </a>
+          </li>
+            <li className="bg-white rounded-lg shadow p-4 hover:shadow-lg transition-shadow duration-200">
             <a href="/test-apps/cientside-database" className="text-lg font-medium text-indigo-600 hover:text-indigo-800">
               Cientside Database
             </a>
