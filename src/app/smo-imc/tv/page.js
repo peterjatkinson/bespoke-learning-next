aaf46d3943b54249e18d916438c91d7ec6a28e01
peterjatkinson <peterjatkinson@gmail.com>
2025-07-04 08:58:36 +0100

    Persona generator minor update

--- src/app/smo-imc/tv/page.js
+++ src/app/smo-imc/tv/page.js
@@ -1,4 +1,5 @@
 
+"use client";
 import React, { useState, useEffect, useRef } from 'react';
 import { Play, Users, TrendingDown, DollarSign, Clock, Tv, Youtube, Zap, Eye, Target, Globe, Timer, Percent } from 'lucide-react';
 
